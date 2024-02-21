d2ea478e983fc12ed9fbfb76f7862862cafffd91
Denis Bortnikov <denis.bortnikov17@gmail.com>
2024-02-21 09:42:19 +0300

    feat: add multer

--- express-api/routes/index.js
+++ express-api/routes/index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+
+const uploadDestination = "uploads";
+
+// Showing where to store files
+const storage = multer.diskStorage({
+  destination: uploadDestination,
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+
+const uploads = multer({ storage: storage });
 
 router.get("/register", (req, res) => {
   res.send("register");
