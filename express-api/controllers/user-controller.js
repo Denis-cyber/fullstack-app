38db25408d280b609e462dde72a72880167fdfbb
Denis Bortnikov <denis.bortnikov17@gmail.com>
2024-03-24 23:13:48 +0300

    start validate register route

--- express-api/controllers/user-controller.js
+++ express-api/controllers/user-controller.js
@@ -1,6 +1,21 @@
+const { prisma } = require("../prisma/prisma-client");
+
 const UserController = {
   register: async (req, res) => {
-    res.send("register");
+    const { email, password, name } = req.body;
+
+    if (!email || !password || !name) {
+      return res.status(400).json({ error: "Все поля обязательны" });
+    }
+
+    try {
+    } catch (error) {
+      const existingUser = await prisma.user.findUnique({ where: { email } });
+
+      if (existingUser) {
+        return res.status(400).json({ error: "Пользователь уже существует" });
+      }
+    }
   },
   login: async (req, res) => {
     res.send("login");
